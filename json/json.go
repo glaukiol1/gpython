6fa20054f8d4aa91e1e8b486c64f11139c682ed9
glaukiol1 <glaukiol1@gmail.com>
2022-03-16 20:34:58 +0100

    some more basic changes

--- json/json.go
+++ json/json.go
@@ -18,6 +18,10 @@ Interaction with JSON.
 
 `
 
+var (
+	JSON_DICT_TYPE = py.NewType("JsonDict", "JSON Key-Value Object")
+)
+
 // serialize StringDict into JSON string
 func json_dumps(self py.Object, args py.Tuple) (py.Object, error) {
 	if len(args) == 0 {
@@ -35,10 +39,40 @@ func json_dumps(self py.Object, args py.Tuple) (py.Object, error) {
 	}
 }
 
+// de-serialize JSON formatted string into JsonDict
+func json_loads(self py.Object, args py.Tuple) (py.Object, error) {
+	if len(args) == 0 {
+		return nil, py.ExceptionNewf(py.TypeError, "Too few arguments")
+	} else {
+		if reflect.TypeOf(args[0]).String() == "py.String" {
+			var result map[string]interface{}
+			arg, err := py.ReprAsString(args[0])
+			if err != nil {
+				return nil, py.ExceptionNewf(py.TypeError, "Failed to parse argument into Go string.")
+			}
+			err = json.Unmarshal([]byte(arg), result)
+			if err != nil {
+				return nil, py.ExceptionNewf(py.KeyError, "Failed to parse JSON")
+			}
+			parsedDict := JSON_DICT_TYPE.Alloc()
+			for k, v := range result {
+				switch c := v.(type) {
+				case string:
+					parsedDict.Dict[k] = py.String(c)
+				}
+			}
+			return parsedDict, nil
+		} else {
+			return nil, py.ExceptionNewf(py.KeyError, "Argument must be of type string")
+		}
+	}
+}
+
 // Initialise the module
 func init() {
 	methods := []*py.Method{
 		py.MustNewMethod("dumps", json_dumps, 0, dumps_doc),
+		py.MustNewMethod("loads", json_loads, 0, ""),
 	}
 
 	py.RegisterModule(&py.ModuleImpl{
